e362f28eefda421ee4d6204b2945304215e066d5
Helen Kim <Helenk490@gmail.com>
2021-01-25 20:47:38 -0500

    1st draft

---
+++ js/app.js
@@ -0,0 +1,12 @@
+  // TERMS & POLICY MODAL
+  $("#termsPolicyBtn").on("click", function() {
+    $("#termsPolicyModal").show();
+  });
+  
+  $("#termsPolicyClose").on("click", function() {
+    $("#termsPolicyModal").hide();
+  });
+  
+  $("#termsPolicyFooterClose").on("click", function() {
+    $("#termsPolicyModal").hide();
+  });
